e857c0ecfba3ad52549ca5aa69831aa98cda77ce
Andrew <ahredzak@gmail.com>
2025-02-17 19:47:48 -0700

    i removed all of the SHT logic and definitoins. i simply wanted to deploy the most basic of functions but format it with a prototype, declaration and instantiation into main. it works and it prints out a message ever second. i guess thats a win :)

--- components/SHT31/SHT31.c
+++ components/SHT31/SHT31.c
@@ -1,48 +1,20 @@
-
-#include "SHT31.h"
-#include <string.h>     // For memset if needed
-#include <stdio.h>      // For debug printing (optional)
-#include "SHT31.h"
-#include "esp_mac.h"
+#include <stdio.h>
+#include "sht31.h"
 #include "esp_log.h"
+#include "driver/i2c_master.h"
+#include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
 
 
-// Define the commands for the SHT31 sensor
-#define SHT31_CMD_READ_STATUS 0xF32D
-#define SHT31_CMD_CLEAR_STATUS 0x3041
-#define SHT31_CMD_SOFT_RESET 0x30A2
-#define SHT31_CMD_HEATER_EN 0x306D
-#define SHT31_CMD_HEATER_DIS 0x3066
-#define SHT31_CMD_MEAS_PERI_H_M 0x2130 // 1 mps, medium repeatability
-
-
-static const char *TAG = "SHT31C";
-
-// prediction: wrapping i2c driver command [i2c_master_transmit_receive]
-// and then calling it in main
-
-
-/*
-sets up variables
-calls function 
-stores data 
-calculates temp and humidity
-write to console 
-*/
 
 
 
-void my_task(void *pvParameters){
-    while(1){
-        printf("first function in sht, HI!  \n");
+
+
+void my_task(void *pvParameters) {
+    while (1) {
+        ESP_LOGI("SHT31_TASK", "the task is running!");
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
-        
 }
-
-
-
-
-
